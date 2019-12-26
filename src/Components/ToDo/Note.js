55d85af59f3743666db909eaad1745473d2a1c6e
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-26 10:01:17 +0900

    Creation Component

--- src/Components/ToDo/Note.js
+++ src/Components/ToDo/Note.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Note extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.noteContent = props.noteContent;
+    this.noteId = props.noteId;
+    this.handleRemoveNote = this.handleRemoveNote.bind(this);
+  }
+
+  handleRemoveNote(id) {
+    this.props.removeNote(id);
+  }
+
+  render(props) {
     return (
-      <div>
-        <h2>Note</h2>
+      <div className="note fade-in">
+        <span
+          className="closebtn"
+          onClick={() => this.handleRemoveNote(this.noteId)}
+        >
+          <i className="fas fa-eraser" />
+        </span>
+        <p className="noteContent">{this.noteContent}</p>
       </div>
     );
   }
 }
+
+Note.propTypes = {
+  noteContent: PropTypes.string
+};
